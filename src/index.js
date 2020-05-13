2a0b129e5e3b386c5331921f0bb6a4b213ebdb29
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-14 01:58:50 +0530

    Frameless

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
-    darkTheme:true,
-    // frame: false,
-    autoHideMenuBar:true,
+    height: 400,
+    darkTheme: true,
+    frame: false,
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
     },
